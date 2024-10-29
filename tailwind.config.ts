63be1901b19bd2b2b6adc1d38113eaae9d6e69a5
nox D invictus <jvcalma24@gmail.com>
2024-10-29 12:36:01 +0700

    feat: change font playwrite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config: Config = {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
       },
+      fontFamily: {
+        pw: ['var(--font-pw)'],
+      },
     },
   },
   plugins: [],
