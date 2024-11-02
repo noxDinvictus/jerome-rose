d3daf586e5322252aa6cbda08aca3e922a2e4e00
nox D invictus <jvcalma24@gmail.com>
2024-11-02 15:25:45 +0700

    feat: create TimeLineContainer component

---
+++ src/components/elements/timeline-container.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+interface IProps {
+  readonly leftContent: ReactNode;
+  readonly rightContent: ReactNode;
+  readonly isHalf?: boolean;
+}
+
+export default function TimeLineContainer({
+  leftContent,
+  rightContent,
+  isHalf,
+}: IProps) {
+  return (
+    <div className="my-5 flex gap-3 md:flex-col">
+      <div className={twMerge('md:w-full', isHalf ? 'w-1/2' : 'w-1/3')}>
+        {leftContent}
+      </div>
+
+      <div className={twMerge('md:w-full', isHalf ? 'w-1/2' : 'w-2/3')}>
+        {rightContent}
+      </div>
+    </div>
+  );
+}
