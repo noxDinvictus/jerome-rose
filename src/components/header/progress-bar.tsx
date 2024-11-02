e168edaf9aba14d36ba601029beb3f8787618d33
nox D invictus <jvcalma24@gmail.com>
2024-11-02 15:26:51 +0700

    feat: create ProgressBar component

---
+++ src/components/header/progress-bar.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+export default function ProgressBar() {
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    const windowHeight = window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
+
+    const progress = (scrollPosition / (documentHeight - windowHeight)) * 100;
+    const progressBar = document.getElementById('progressBar') as HTMLElement;
+
+    if (progressBar) progressBar.style.width = `${progress}%`;
+  };
+
+  document.addEventListener('scroll', handleScroll);
+
+  return (
+    <div
+      className="progress-bar-striped fixed left-0 top-0 z-50 h-1 w-0 bg-groom-100 [transition:width_0.5s_ease]"
+      id="progressBar"
+    />
+  );
+}
