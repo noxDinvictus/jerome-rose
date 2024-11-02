dbc589c8e7ecc76005f8b8133afac3b8bfd4bf7e
nox D invictus <jvcalma24@gmail.com>
2024-11-02 12:01:41 +0700

    feat: implement main component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
+import Main from '@/components/templates/mian';
+
 export default function Home() {
-  return (
-    <>
-      <main>
-        <p className="font-pw text-3xl text-red-400">
-          hereas disregard and contempt for human rights have resulted
-        </p>
-      </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
-        this is a footer
-      </footer>
-    </>
-  );
+  return <Main />;
 }
