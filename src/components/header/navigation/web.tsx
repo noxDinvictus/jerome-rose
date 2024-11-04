8da3805af19218547dbf36a36776b0f833d86c98
nox D invictus <jvcalma24@gmail.com>
2024-11-05 06:51:41 +0700

    feat: create NavigationWeb component

---
+++ src/components/header/navigation/web.tsx
@@ -0,0 +1,16 @@
+import { I } from '@/interface';
+import NavButton from './button';
+
+export function NavigationWeb({ navigate }: Readonly<I.Navigate>) {
+  return (
+    <nav className="absolute left-5 top-[40%] md:top-[30%] md:hidden">
+      <div className="flex h-full flex-col gap-5 text-white">
+        <NavButton
+          navigate={navigate}
+          className="animate__animated animate__lightSpeedInLeft text-left"
+          delay
+        />
+      </div>
+    </nav>
+  );
+}
