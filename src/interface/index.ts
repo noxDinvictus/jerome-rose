8c85749605b3bc92526bd87cf0bf791c4858eab4
nox D invictus <jvcalma24@gmail.com>
2024-11-05 06:23:47 +0700

    feat: create Navigation and Navigate interface

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -4,4 +4,14 @@ export namespace I {
   export interface Child {
     readonly children: ReactNode;
   }
+
+  export interface Navigation {
+    name: string;
+    id: string;
+    isLink: boolean;
+  }
+
+  export interface Navigate {
+    navigate(data: Readonly<I.Navigation>): void;
+  }
 }
