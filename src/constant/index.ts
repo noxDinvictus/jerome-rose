e342cb820e1853540c3ad7fc20b54683c28c5894
nox D invictus <jvcalma24@gmail.com>
2024-11-09 14:03:40 +0700

    feat: add new constants

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -2,44 +2,58 @@ import { I } from '@/interface';
 
 export const c = {
   date: '2024-12-20T00:00:00.000Z', // 20 December 2024
+  ceremony: {
+    place: 'Embassy Of The Republic Of The Philippines',
+    time: '07:00 - 08:00',
+    location: 'Phnom, 182 Preah Norodom Blvd (41), Penh 12301',
+    googleMap: 'https://maps.app.goo.gl/BF2u3819a8RTR3oG6',
+  },
+  reception: {
+    place: "Bride's house",
+    time: '11:00 - 14:00',
+    location: 'Bahay lang namin',
+    googleMap: 'https://maps.app.goo.gl/BF2u3819a8RTR3oG6',
+  },
 };
 
 export const headerId = {
-  theProposal: 'the-proposal',
-  ourStory: 'our-story',
   preNup: '/',
+  dressCode: 'dressCode',
+  giftGuide: 'giftGuide',
+  theProposal: 'the-proposal',
+  reception: 'reception',
+  ourStory: 'our-story',
 };
 
 export const headerNav: I.Navigation[] = [
   {
     name: 'Pre-nup',
     id: '/',
-    isLink: false,
+    link: '/',
   },
-  {
-    name: 'Reception Venue',
-    id: 'reception',
-    isLink: false,
-  },
-  {
-    name: 'Dress Code',
-    id: 'dressCode',
-    isLink: false,
-  },
-  {
-    name: 'Gift Guide',
-    id: 'giftGuide',
-    isLink: false,
-  },
-
   {
     name: 'The Proposal',
-    id: 'theProposal',
-    isLink: true,
+    id: headerId.theProposal,
+    link: '/wedding/the-proposal',
   },
   {
     name: 'Our Story',
     id: headerId.ourStory,
-    isLink: true,
+    link: '/wedding/our-story',
+  },
+];
+
+export const landingPageSubNav: I.Navigation[] = [
+  {
+    name: 'Reception Venue',
+    id: headerId.reception,
+  },
+  {
+    name: 'Dress Code',
+    id: headerId.dressCode,
+  },
+  {
+    name: 'Gift Guide',
+    id: headerId.giftGuide,
   },
 ];
