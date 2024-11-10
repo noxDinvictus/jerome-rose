c5fb247ca606cb8824f92c74ee1615057d35aad0
nox D invictus <jvcalma24@gmail.com>
2024-11-10 20:51:49 +0700

    style: add z index

--- src/components/header/navigation/web.tsx
+++ src/components/header/navigation/web.tsx
@@ -3,7 +3,7 @@ import NavButton from './button';
 
 export function NavigationWeb({ navigate }: Readonly<I.Navigate>) {
   return (
-    <nav className="absolute left-5 top-[50%] md:top-[30%] md:hidden">
+    <nav className="absolute left-5 top-[50%] z-20 md:top-[30%] md:hidden">
       <div className="flex h-full flex-col gap-5 text-white">
         <NavButton
           navigate={navigate}
