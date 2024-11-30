9933400817912745a923560c8efc7c7ae24b3fc6
nox D invictus <jvcalma24@gmail.com>
2024-11-30 14:44:55 +0700

    style: update dimensions

--- src/components/shared/svg/jerome-and-rose.svg.tsx
+++ src/components/shared/svg/jerome-and-rose.svg.tsx
@@ -1,8 +1,7 @@
 export function JeromeAndRose() {
   return (
     <svg
-      width="auto"
-      height="auto"
+      style={{ width: '100%', height: '100%' }}
       viewBox="0 0 1845 283"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
