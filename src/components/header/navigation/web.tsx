2a1d2c9163e0398572cb005f7082474f37e0c754
nox D invictus <jvcalma24@gmail.com>
2024-11-30 14:44:37 +0700

    style: fix spacing depending on route

--- src/components/header/navigation/web.tsx
+++ src/components/header/navigation/web.tsx
@@ -1,9 +1,18 @@
+import { headerId } from '@/constant';
 import { I } from '@/interface';
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
 import NavButton from './button';
 
 export function NavigationWeb({ navigate }: Readonly<I.Navigate>) {
+  const route = usePathname();
+
+  const isWedding = route === headerId.wedding ? 'top-[40%] ' : 'top-[50%] ';
+
   return (
-    <nav className="absolute left-5 top-[50%] z-20 md:top-[30%] md:hidden">
+    <nav
+      className={clsx('absolute left-5 z-20 md:top-[30%] md:hidden', isWedding)}
+    >
       <div className="flex h-full flex-col gap-5 text-white">
         <NavButton
           navigate={navigate}
